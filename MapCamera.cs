a2f7e88a6161fb899b18373092dd99098ff348c4
Chloe Janko <chloenoelle98@gmail.com>
2017-05-31 21:23:09 -0700

    Add files via upload

---
+++ MapCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCamera : MonoBehaviour {
+
+    public Transform target;
+    public float smootheTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    
+
+    void FixedUpdate() {
+        Vector3 goalPos = target.position;
+        goalPos.y = transform.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smootheTime);
+    }
+}
---
+++ Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 75 * Time.deltaTime, 0);
+    }
+}
